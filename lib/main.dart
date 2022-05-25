fc614a0640c050fc35e38b9b4744b76a05c08225
jeremias ramirez <jereramirez499@gmail.com>
2022-05-25 03:15:06 -0400

    init

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:ebuyapp/pages/start/start.page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        initialRoute: "start/",
+        title: "Flutter ebuy",
+        routes: {"start/": (context) => const StartPage()});
+  }
+}
---
+++ lib/pages/start/start.page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({Key? key}) : super(key: key);
+
+  @override
+  StartPageFul createState() => StartPageFul();
+}
+
+class StartPageFul extends State<StartPage> {
+  @override
+  Widget build(context) {
+    return const Text("Hello");
+  }
+}
