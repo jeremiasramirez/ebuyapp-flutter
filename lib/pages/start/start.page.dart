80928f1706536abd663afc33995eb87d163951c7
jeremias ramirez <jereramirez499@gmail.com>
2022-05-25 04:48:42 -0400

    agregado de imagenes para decorar

--- lib/pages/start/start.page.dart
+++ lib/pages/start/start.page.dart
@@ -17,7 +17,7 @@ class StartPageFul extends State<StartPage> {
             body: Center(
                 child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [logoForStart(), titleForLogo()],
+          children: [logoForStart(), rowObjects(), loadingForLogo()],
         ))));
   }
 
@@ -25,28 +25,84 @@ class StartPageFul extends State<StartPage> {
     // ignore: avoid_unnecessary_containers
     return Container(
         margin: const EdgeInsets.only(top: 20),
-        child: FadeInUp(
+        child: FadeIn(
             child: const CircularProgressIndicator(
-                strokeWidth: 2.5, color: Colors.redAccent)));
+                strokeWidth: 1.5, color: Colors.redAccent)));
   }
 
   Container titleForLogo() {
     // ignore: avoid_unnecessary_containers
     return Container(
-        margin: const EdgeInsets.only(top: 25.0, left: 0, right: 0, bottom: 0),
-        child: FadeInUp(
+        margin: const EdgeInsets.only(top: 0.0, left: 0, right: 0, bottom: 20),
+        child: FadeIn(
             child: const Text("Ebuy",
                 style: TextStyle(
-                    fontSize: 20,
+                    fontSize: 22,
                     fontWeight: FontWeight.w500,
                     fontFamily: "ubuntu",
                     color: Colors.redAccent))));
   }
 
+  Widget rowObjects() {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      Container(
+          width: 50,
+          height: 50,
+          padding: const EdgeInsets.only(top: 8, bottom: 8),
+          decoration: BoxDecoration(
+              color: Colors.grey[100], borderRadius: BorderRadius.circular(10)),
+          child: Opacity(
+            child: FadeIn(
+                child: const Image(
+                    width: 30,
+                    image: AssetImage("assets/images/smartphone.png"))),
+            opacity: .1,
+          )),
+      Container(
+          width: 50,
+          height: 50,
+          padding: const EdgeInsets.only(top: 8, bottom: 8),
+          decoration: BoxDecoration(
+              color: Colors.grey[100], borderRadius: BorderRadius.circular(10)),
+          child: Opacity(
+            child: FadeIn(
+                child: const Image(
+                    width: 30, image: AssetImage("assets/images/monitor.png"))),
+            opacity: .1,
+          )),
+      Container(
+          width: 50,
+          height: 50,
+          padding: const EdgeInsets.only(top: 8, bottom: 8),
+          decoration: BoxDecoration(
+              color: Colors.grey[100], borderRadius: BorderRadius.circular(10)),
+          child: Opacity(
+            child: FadeIn(
+                child: const Image(
+                    width: 30, image: AssetImage("assets/images/heart.png"))),
+            opacity: .1,
+          )),
+      Container(
+          width: 50,
+          height: 50,
+          padding: const EdgeInsets.only(top: 8, bottom: 8),
+          decoration: BoxDecoration(
+              color: Colors.grey[100], borderRadius: BorderRadius.circular(10)),
+          child: Opacity(
+            child: FadeIn(
+                child: const Image(
+                    width: 30,
+                    image: AssetImage("assets/images/headphones.png"))),
+            opacity: .1,
+          ))
+    ]);
+  }
+
   Container logoForStart() {
     // ignore: avoid_unnecessary_containers
     return Container(
-        child: FadeInDown(
+        margin: const EdgeInsets.only(top: 0.0, left: 0, right: 0, bottom: 0),
+        child: FadeIn(
             child: const Image(
                 width: 160, image: AssetImage("assets/images/logo.png"))));
   }
